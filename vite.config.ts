b25fad41386f9e7032ba6af69e4ab62155fc408d
Harshal <harshal.madgulkar725@gmail.com>
2025-11-05 15:30:13 +0530

    Add react-router v7.9.5; update tsconfig path aliases and Vite config
    - Added react-router 7.9.5 to dependencies in package.json and package-lock.json
    - Updated tsconfig.app.json to include path aliases for project folders
    - Configured Vite resolve.alias to match TypeScript paths for cleaner imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import path, { resolve } from 'path';
+
+const root = resolve(__dirname, 'src');
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@root': root,
+      '@app': path.resolve(root, 'app'),
+      '@assets': path.resolve(root, 'assets'),
+      '@pages': path.resolve(root, 'pages'),
+      '@components': path.resolve(root, 'components'),
+      '@features': path.resolve(root, 'features'),
+      '@utils': path.resolve(root, 'utils'),
+      '@hooks': path.resolve(root, 'hooks'),
+    }
+  }
 });
