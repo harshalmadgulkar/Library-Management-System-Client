3513b11bf5fd9476468bc4b6709e433a32f9f364
Harshal <harshal.madgulkar725@gmail.com>
2025-11-20 09:16:32 +0530

    feat(auth): implement OTP verification page with form and feedback
    - Added OTP.tsx with controlled input for OTP code and email param from router
    - Integrated Redux otpVerification action, success/error toast notifications, and resetting auth state
    - Provided responsive layout, branding, and navigation between register and OTP screens
    - Redirect authenticated users to home page

--- src/pages/OTP.tsx
+++ src/pages/OTP.tsx
@@ -1,7 +1,106 @@
+import { useAppDispatch, useAppSelector } from "@store/storeHooks";
+import { useEffect, useState } from "react";
+import { Link, Navigate, useParams } from "react-router";
+
+import logo from "@assets/black-logo.png";
+import logo_with_title from "@assets/logo-with-title.png";
+import { toast } from "sonner";
+import { otpVerification, resetAuthSlice } from "@store/slices/authSlice";
 
 const OTP = () => {
+    const { email } = useParams();
+    const [otp, setOtp] = useState("");
+
+    const dispatch = useAppDispatch();
+    const { loading, error, isAuthenticated, message } = useAppSelector(state => state.auth);
+
+    const handleOtpVerification = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (email) {
+            dispatch(otpVerification(email, otp));
+        } else {
+            toast.error("Email is required for OTP verification.");
+        }
+    };
+
+    useEffect(() => {
+        if (message) {
+            toast.success(message);
+        }
+        if (error) {
+            toast.error(error);
+            dispatch(resetAuthSlice());
+        }
+    }, [dispatch, isAuthenticated, error, loading]);
+
+    if (isAuthenticated) {
+        return <Navigate to={"/"} />;
+    }
+
     return (
-        <div>OTP</div>
+        <div className="flex flex-col justify-center md:flex-row h-screen">
+            {/* Left Side */}
+            <div className="w-full md:w-1/2 flex items-center justify-center bg-white p-8 relative">
+                <Link to={"/register"}
+                    className="border-2 border-black rounded-3xl font-bold w-52 py-2 px-4 fixed top-10 
+                    -left-28 hover:bg-black hover:text-white transition duration-300 text-end"
+                >
+                    Back
+                </Link>
+                <div className="max-w-sm w-full">
+                    <div className="flex justify-center mb-12">
+                        <div className="rounded-full flex items-center justify-center">
+                            <img src={logo} alt="logo" className="h-24 w-auto" />
+                        </div>
+                    </div>
+                    <h1 className="text-4xl font-medium text-center mb-12 overflow-hidden">
+                        Check your Mailbox
+                    </h1>
+                    <p className="text-gray-800 text-center mb-12">
+                        Please enter the OTP to proceed
+                    </p>
+
+                    <form onSubmit={handleOtpVerification}>
+                        <div className="mb-4">
+                            <input type="number"
+                                value={otp}
+                                onChange={(e) => setOtp(e.target.value)}
+                                placeholder="OTP"
+                                className="w-full px-4 py-3 border border-black rounded-md focus:outline-none
+                                 [appearance:textfield] 
+                                 [&::-webkit-outer-spin-button]:appearance-none 
+                                 [&::-webkit-inner-spin-button]:appearance-none"
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="border-2 mt-5 border-black w-full font-semibold bg-black 
+                            text-white py-2 rounded-lg hover:bg-white hover:text-black transition cursor-pointer"
+                        >
+                            VERIFY
+                        </button>
+                    </form>
+                </div>
+            </div>
+            {/* Right Side */}
+            <div
+                className="hidden w-full md:w-1/2 bg-black text-white md:flex flex-col items-center
+             justify-center p-8 rounded-tl-[80px] rounded-bl-[80px]"
+            >
+                <div className="text-center h-[400px]">
+                    <div className="flex justify-center mb-12">
+                        <img src={logo_with_title} alt="logo" className="mb-12 h-44 w-auto" />
+                    </div>
+                    <p className="text-gray-300 mb-12">New to our platform? Sign Up Now.</p>
+                    <Link
+                        to={"/register"}
+                        className="border-2 mt-5 border-white w-full font-semibold bg-black 
+                            text-white py-2 px-8 rounded-lg hover:bg-white hover:text-black transition">
+                        SIGN UP
+                    </Link>
+                </div>
+            </div>
+        </div>
     );
 };
 
